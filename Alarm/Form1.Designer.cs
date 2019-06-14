af817e2d7c4ef34f5ca7cbb53acef587329dada8
siwonlim <dbu7422@gmail.com>
2019-06-15 03:00:00 +0900

    feat : made a simple alarm clock wins-app for me
    A major cs file is Form1.cs.

---
+++ Alarm/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Alarm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.count = new System.Windows.Forms.Label();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.time = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // count
+            // 
+            this.count.AutoSize = true;
+            this.count.Font = new System.Drawing.Font("Footlight MT Light", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.count.Location = new System.Drawing.Point(54, 57);
+            this.count.Name = "count";
+            this.count.Size = new System.Drawing.Size(269, 68);
+            this.count.TabIndex = 0;
+            this.count.Text = "00:00:00";
+            // 
+            // startBtn
+            // 
+            this.startBtn.Font = new System.Drawing.Font("Corbel", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startBtn.Location = new System.Drawing.Point(66, 228);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(123, 54);
+            this.startBtn.TabIndex = 1;
+            this.startBtn.Text = "START";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.button1_Click);
+            this.startBtn.KeyDown += new System.Windows.Forms.KeyEventHandler(this.startBtn_KeyDown);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Font = new System.Drawing.Font("Corbel", 9F);
+            this.stopBtn.Location = new System.Drawing.Point(196, 228);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(120, 54);
+            this.stopBtn.TabIndex = 2;
+            this.stopBtn.Text = "STOP";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.button2_Click);
+            this.stopBtn.KeyDown += new System.Windows.Forms.KeyEventHandler(this.stopBtn_KeyDown);
+            // 
+            // time
+            // 
+            this.time.Font = new System.Drawing.Font("굴림", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.time.Location = new System.Drawing.Point(63, 161);
+            this.time.Multiline = true;
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(254, 34);
+            this.time.TabIndex = 3;
+            this.time.KeyDown += new System.Windows.Forms.KeyEventHandler(this.time_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 325);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.count);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label count;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.TextBox time;
+    }
+}
+
---
+++ Alarm/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alarm
+{
+    public partial class Form1 : Form
+    {
+        static int MAX = 3;
+        int[] hms;
+
+        public Form1()
+        {
+            InitializeComponent();
+            time.Text = "";
+            hms = new int[MAX];
+            time.Select();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(hms[2] > 0)
+            {
+                hms[2]--;
+            }
+            else
+            {
+                if (hms[0]+ hms[1]+ hms[2] == 0)
+                {
+                    timer1.Stop();
+                    timer1.Enabled = false;
+                    refresh();
+                    MessageBox.Show("끝!", "시간종료", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                    return;
+                }
+
+                if (hms[1] > 0) hms[1]--;
+                else if (hms[0] > 0)
+                {
+                    hms[0]--;
+                    hms[1] = 59;
+                }
+                hms[2] = 59;
+            }
+            tick();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(time.Text)) return;
+            else
+            {
+                int t = Int32.Parse(time.Text);
+                hms[0] = t / 60;
+                hms[1] = t % 60;
+                hms[2] = 0;
+                tick();
+                time.Text = "";
+                timer1.Enabled = true;
+                timer1.Start();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            refresh();
+        }
+
+        private void time_KeyDown(object sender, KeyEventArgs e)
+        {
+            Form1_KeyDown(sender, e);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.KeyCode == Keys.Enter)
+                {
+                    button1_Click(sender, e);
+                }
+                else if (e.KeyCode == Keys.Escape)
+                {
+                    this.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("catch : " + ex.Message);
+            }
+        }
+
+        private void stopBtn_KeyDown(object sender, KeyEventArgs e)
+        {
+            Form1_KeyDown(sender, e);
+        }
+
+        private void startBtn_KeyDown(object sender, KeyEventArgs e)
+        {
+            Form1_KeyDown(sender, e);
+        }
+
+        private void tick()
+        {
+            String []hms_txt = new String[3];
+            for(int i = 0; i < MAX; i++)
+            {
+                if (hms[i] / 10 == 0) hms_txt[i] = "0" + hms[i].ToString();
+                else hms_txt[i] = hms[i].ToString();
+            }
+            count.Text = hms_txt[0] + ":" + hms_txt[1] + ":" + hms_txt[2];
+        }
+
+        private void refresh()
+        {
+            time.Text = "";
+            hms[0] = hms[1] = hms[2] = 0;
+            count.Text = "00:00:00";
+            timer1.Stop();
+        }
+    }
+}
